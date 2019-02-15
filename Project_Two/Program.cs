c09d97c7f592b2e593b335f1396d72273c963a08
Anders  Nelson <nelandf@dunwoody.edu>
2019-02-14 20:47:31 -0600

    updated super bowl class and read objects from file

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
 
 namespace Project_Two
 {
@@ -9,10 +12,46 @@ namespace Project_Two
             /**Your application should allow the end user to pass end a file path for output 
             * or guide them through generating the file.
             **/
+            string[] arrayOfValues;
+            List<SuperBowl> listOfSuperBowls = new List<SuperBowl>();
 
-            Console.WriteLine("testing git");
+            string filePath = Directory.GetCurrentDirectory();
+            string stepBackOne = Directory.GetParent(filePath).ToString();
+            string stepBackTwo = Directory.GetParent(stepBackOne).ToString();
+            string stepBackThree = Directory.GetParent(stepBackTwo).ToString();
+            //Console.WriteLine(stepBackThree);
 
-           
+            string adjustedFilePath = $@"{stepBackThree}\Super_Bowl_Project.csv";
+            //Console.WriteLine(adjustedFilePath);
+
+            if (File.Exists(adjustedFilePath))
+            {
+                FileStream file = new FileStream(adjustedFilePath, FileMode.Open, FileAccess.Read);
+                StreamReader read = new StreamReader(file);
+
+                while (!read.EndOfStream)
+                {
+                    try
+                    {
+                        arrayOfValues = read.ReadLine().Split(',');
+                        listOfSuperBowls.Add(new SuperBowl(arrayOfValues[0], arrayOfValues[1], Int32.Parse(arrayOfValues[2]), arrayOfValues[3], arrayOfValues[4],
+                            arrayOfValues[5], Int32.Parse(arrayOfValues[6]), arrayOfValues[7], arrayOfValues[8], arrayOfValues[9], Int32.Parse(arrayOfValues[10]), arrayOfValues[11], arrayOfValues[12], arrayOfValues[13]));
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                read.Close();
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("can't find file");
+            }
+
+            
+            listOfSuperBowls.ForEach(i => Console.WriteLine(i.ToString()));
         }
     }
 }
---
+++ Project_Two/SuperBowl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Two
+{
+    class SuperBowl
+    {
+        public string Date { get; set; }
+        public int Attendance { get; set; }
+        public string SuperBowlRomanNumeral { get; set; }
+        public int SuperBowlNumber { get; set; }
+        public string City { get; set;}
+        public string State { get; set; }
+        public string Stadium { get; set; }
+        public string WinningTeamName { get; set; }
+        public string LosingTeamName { get; set; }
+        public string WinningQuarterback { get; set; }
+        public string LosingQuarterback { get; set; }
+        public string WinningCoach { get; set; }
+        public string LosingCoach { get; set; }
+        public string MVP { get; set; }
+        public int WinningTeamPoints { get; set; }
+        public int LosingTeamPoints { get; set; }
+
+        //Default Constructor
+        public SuperBowl()
+        {
+
+        }
+
+        public SuperBowl(string Date, string SuperBowlRomanNumeral, int Attendance
+            , string WinningQuarterback, string WinningCoach, string WinningTeamName, 
+            int WinningTeamPoints, string LosingQuarterback, string LosingCoach, string LosingTeamName
+            , int LosingTeamPoints, string MVP, string Stadium, string State)
+        {
+            this.Date = Date;
+            this.SuperBowlRomanNumeral = SuperBowlRomanNumeral;
+            this.Attendance = Attendance;
+            this.WinningQuarterback = WinningQuarterback;
+            this.WinningCoach = WinningCoach;
+            this.WinningTeamName = WinningTeamName;
+            this.WinningTeamPoints = WinningTeamPoints;
+            this.LosingQuarterback = LosingQuarterback;
+            this.LosingCoach = LosingCoach;
+            this.LosingTeamPoints = LosingTeamPoints;
+            this.MVP = MVP;
+            this.Stadium = Stadium;
+            this.State = State;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Date: {0} \nSuper Bowl Number: {1} \nAttendance: {2} \nWinning Quarterback: {3} \nWinning Coach: {4}\nWinning Team: {5}\n", Date, SuperBowlRomanNumeral, Attendance, WinningQuarterback, WinningCoach, WinningTeamName);
+        }
+
+
+    }
+}
